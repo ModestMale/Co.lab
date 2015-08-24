113a708f551457e7883bb48eea8629eeaf9927b3
ModestMale <remi.bouskila@gmail.com>
2015-08-24 20:29:59 +1000

    updates

--- js/index.js
+++ js/index.js
@@ -2,3 +2,21 @@ $(".plusminus").click(function(){
 	$(this).parent().toggleClass("open");
 
 })
+
+$(".headericon").click(function(){
+	$(this).parent().toggleClass("showsearch");
+})
+
+$(".findcollabimg").click(function(){
+	$(this).parent().toggleClass("imgorange");
+})
+
+$("#profilemenu1").click(function(){
+	$(this).parent().toggleClass("open");
+
+})
+
+$("#profilemenu2").click(function(){
+	$(this).parent().toggleClass("open");
+
+})
